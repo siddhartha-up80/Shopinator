effaaaf084f8cc1b57048e6a3dcece2f7caa623a
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-07-03 20:04:14 +0530

    Create Store Form State Added
    added create store form state using zustand and modals, created hooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] })
+import { ModalProvider } from "@/providers/modal-provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Shopinator',
-  description: 'Admin Dashboard',
-}
+  title: "Shopinator",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
